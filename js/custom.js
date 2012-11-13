d1bb2fb922ff1dfbe386bfea6211a9777a4043ff
Derek <heyderek@dereknelson.net>
2012-11-13 12:06:50 -0800

    added fonts.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,13 @@
 $(window).load(function() {
   //Activate Flexslider Carousel
-  $('.flexslider').flexslider({
-    animation: "slide"
+  $('#flexslider1').flexslider({
+    animation: "slide",
+    controlNav: false
+  });
+  $('#flexslider2').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    slideshow: false,
+    controlNav: false,
   }); 
 });
