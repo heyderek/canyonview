9c04ef283d2bfe683a360b471adb956408d036a7
Derek <heyderek@dereknelson.net>
2012-11-14 11:51:52 -0800

    Fixed charset for index, and added animations for the captions.

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,25 @@
-$(window).load(function() {
-  //Activate Flexslider Carousel
+$(document).ready(function() {
+  //Hide all captions on Load
+  $('.caption').hide();  
+  //Activate Flexslider Slideshow
   $('#flexslider1').flexslider({
     animation: "slide",
-    controlNav: false
+    controlNav: false,
+    start: function(animatingTo){
+      $('.caption').slideDown(300);
+    },
+    after: function(animatingTo){
+      $('.caption').slideDown(300);
+    },
+    before: function(animatingTo){
+      $('.caption').hide();
+    },
   });
+  //Activate Flexslider Carousel
   $('#flexslider2').flexslider({
     animation: "slide",
     animationLoop: false,
     slideshow: false,
-    controlNav: false,
+    controlNav: false
   }); 
 });
