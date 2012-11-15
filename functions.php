d7878bbb2fa31b049c98a3028c1961c19b66932f
Derek <heyderek@dereknelson.net>
2012-11-14 17:21:56 -0800

    added menus and other wordpress-related items.

--- functions.php
+++ functions.php
@@ -1,19 +1,21 @@
 <?php
 
-//Add Primary Menu Area
-register_nav_menus( array(
-  'primary' => __( 'Primary Menu', 'cvw' ),
-  ) 
-);
 
 //Add Sidebar Widget Area
 function cv_widgets() {
   register_sidebar( array(
-    'name' => __( 'Sidebar Posts', 'cvw' ),
+    'name' => __( 'Sidebar', 'cvw' ),
     'id' => 'sidebar',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => "</aside>",
     'before_title' => '<h3 class="widget-title">',
     'after_title' => '</h3>'
   ) );
-add_action('init', ''); 
+}
+add_action('init', 'cv_widgets'); 
+
+//Add Primary Menu Area
+register_nav_menus( array(
+  'primary' => __( 'Primary Menu', 'bnb' ),
+  ) 
+);
