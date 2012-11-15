7cf0ab4d4ded36b3d18427d9c428252f0003e049
Derek <heyderek@dereknelson.net>
2012-11-14 16:38:04 -0800

    added basic Wordpress files.

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width" />
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css" title="style.css" type="text/css" media="screen" charset="utf-8">
+    <!--[if lt IE 9]><script src="js/html5.js" charset="utf-8"></script><![endif]-->
+    <script src="js/jquery.js" charset="utf-8"></script>
+    <script src="js/prefixfree.min.js" charset="utf-8"></script>
+    <script src="js/flexslider.js" charset="utf-8"></script>
+    <script src="js/custom.js" charset="utf-8"></script>
+    <title>Canyon View Family Eye Care</title>
+  </head>
+  <body>
+    <div id="page">
+      <section id="header_wrap">
+        <header class="wrapper">
+          <hgroup id="branding">
+            <h1>Canyon View Family Eye Care</h1>
+            <h2>Dr. Karl Czirr</h2>
+            <a id="logo" href="#home"><img src="images/canyonview_logo.png" /></a>
+          </hgroup><!-- /#branding -->
+          <nav>
+            <ul id="access">
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li>
+                <a href="#">Link</a>
+                <ul>
+                  <li><a href="#">link</a></li>
+                  <li><a href="#">link</a></li>
+                  <li><a href="#">link</a></li>
+                  <li><a href="#">link</a></li>
+                </ul>
+              </li>
+            </ul><!-- /#access -->
+          </nav>
+          <div id="head_location">
+            <ul>
+              <li><address>1234 Anywhere, WA</address></li>
+              <li><p>509-123-4567</p></li>
+            </ul>
+          </div>
+        </header>
+      </section><!-- /#header_wrap -->
