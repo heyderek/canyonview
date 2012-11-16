fe55865fa456d0f085aaf8c02a50971b2f16bf96
Derek <heyderek@dereknelson.net>
2012-11-16 11:34:13 -0800

    Added Single.php file template, made necessary layout adjustments.

---
+++ single.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+      <section class="subheader"></section>
+      <section class="content">
+        <div class="wrapper">
+          <div id="primary">
+            <?php while ( have_posts() ) : the_post(); ?>
+              <article>
+                <h3><?php the_title(); ?></h3>
+                <h4><?php the_date('F, Y'); ?></h4>
+                <?php the_content(); ?>
+              </article><!-- /.post -->
+            <?php endwhile; ?>
+          </div><!-- /#primary -->
+        <?php get_sidebar(); ?>
+        </div><!-- ./wrapper -->
+      </section><!-- /.content, /#front_page -->
+<?php get_footer(); ?>
