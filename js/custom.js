1e7e203ec310e8e17112d13df00edabcc304a4df
Derek <heyderek@dereknelson.net>
2012-11-19 16:46:15 -0800

    Adding adaptive navigation elements.

--- js/custom.js
+++ js/custom.js
@@ -22,4 +22,21 @@ $(document).ready(function() {
     slideshow: false,
     controlNav: false
   }); 
+  
+  var winWidth = $(window).width();
+  if ( winWidth < 959 ) {
+    $('nav ul').hide();
+    $('nav').prepend('<div id="here"><button>click me</button></div>');
+    if($('nav ul').is(':hidden')) {
+      $('#here button').click(function(){
+        $('nav ul').slideDown();
+      });
+    } else {
+      $('#here button').click(function(){
+        $('nav ul').slideUp();
+      });
+    }
+  } else {
+  
+  };
 });
